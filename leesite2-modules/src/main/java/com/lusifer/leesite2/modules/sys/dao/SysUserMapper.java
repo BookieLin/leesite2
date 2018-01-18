53abeaa8952f409cb5465edecbced24fdbe5c7a6
Lusifer <topsale@vip.qq.com>
2018-01-18 15:13:03 +0800

    SSM 整合完毕

---
+++ leesite2-modules/src/main/java/com/lusifer/leesite2/modules/sys/dao/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.lusifer.leesite2.modules.sys.dao;
+
+import com.lusifer.leesite2.modules.sys.entity.SysUser;
+import tk.mybaits.mapper.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
