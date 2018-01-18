740bc68a556efd27841fafa32a6153f55b8030e2
Lusifer <topsale@vip.qq.com>
2018-01-18 15:07:04 +0800

    集成 tk.mybaits 自动生成

---
+++ leesite2-modules/src/main/java/com/lusifer/leesite2/modules/sys/security/FormAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.lusifer.leesite2.modules.sys.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FormAuthenticationFilter extends org.apache.shiro.web.filter.authc.FormAuthenticationFilter {
+}
---
+++ leesite2-modules/src/main/java/com/lusifer/leesite2/modules/sys/security/UsernamePasswordToken.java
@@ -0,0 +1,4 @@
+package com.lusifer.leesite2.modules.sys.security;
+
+public class UsernamePasswordToken extends org.apache.shiro.authc.UsernamePasswordToken {
+}
